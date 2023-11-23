cfa222df8b0809c97f9c7cae694ed45d168ed1d3
ARSH5588 <arsh013@hotmail.com>
2023-11-22 22:11:20 -0500

    Started home page

--- Views/Home/home.php
+++ Views/Home/home.php
@@ -2,10 +2,13 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="refresh" content="10">
     <title>Home</title>
+    <link rel="stylesheet" type="text/css" href="Views/Styles/shared.css">
+    <link rel="stylesheet" type="text/css" href="Views/Styles/navbar.css">
 </head>
 <body>
-
+<?php
+include_once 'Views/Shared/navbar.php';
+?>
 </body>
 </html>
--- Views/Shared/navbar.php
+++ Views/Shared/navbar.php
@@ -1,21 +1,31 @@
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="refresh" content="10">
-        <title>Register</title>
-        <link rel="stylesheet" type="text/css" href="Views/Styles/shared.css">
-        <link rel="stylesheet" type="text/css" href="Views/Styles/navbar.css">
-    </head>
-    <body>
-        <nav>
-            <section class="profileSection width100Percent">
 
-            </section>
+<nav>
+    <section class="profileSection">
+        <img src="Views/Images/profile.png" alt="profile">
+    </section>
 
-            <section class="navBar width100Percent">
-                <input type="button">
-            </section>
-        </nav>
-    </body>
-</html>
+    <section class="navBar">
+        <section>
+            <div>
+                <h2>LOGO</h2>
+            </div>
+        </section>
+        <section>
+            <div>
+                <button>Home</button>
+            </div>
+            <div>
+                <button>Services</button>
+            </div>
+            <div>
+                <button>FAQ</button>
+            </div>
+            <div>
+                <button>Contact Us</button>
+            </div>
+            <div>
+                <button>****</button>
+            </div>
+        </section>
+    </section>
+</nav>
