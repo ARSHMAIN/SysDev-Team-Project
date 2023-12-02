abde3c63f668c858a4c80400c1f04274f22274b2
moonflaked <loganluo288@gmail.com>
2023-12-01 23:45:12 -0500

    Changed center tag to textalign css property

--- Views/Home/home.php
+++ Views/Home/home.php
@@ -38,9 +38,9 @@ include_once 'Views/Shared/navbar.php';
 -->
     <div class="padding">
         <h1>Slogan</h1>
-        <center>
+        <div class="textAlignCenter">
             <img src="Views/Images/snake_dummy_image.jpeg" alt="snake image">
-        </center>
+        </div>
         <h2>About Us</h2>
         <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
         <h2>News</h2>
