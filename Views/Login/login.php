52ff892c18e58dbd07906b6ad5d734eb6630b818
moonflaked <loganluo288@gmail.com>
2023-12-01 21:11:16 -0500

    Finished login view page

--- Views/Login/login.php
+++ Views/Login/login.php
@@ -4,21 +4,60 @@ session_unset();
 session_destroy();
 ?>
 <html>
-<head>
-    <title>Login</title>
-    <link rel="stylesheet" type="text/css" href="Views/Styles/shared.css">
-    <link rel="stylesheet" type="text/css" href="Views/Styles/navbar.css">
-</head>
-<body>
-<?php
-include_once 'Views/Shared/navbar.php';
-?>
-<form action="/?controller=login&action=validation" method="POST">
-    <label for="email">Email</label>
-    <input id="email" type="text" name="email" required><br>
-    <label for="password">Password</label>
-    <input id="password" type="password" name="password" required>
-    <input type="submit" name="submit" value="Login">
-</form>
-</body>
+    <head>
+        <title>Login</title>
+        <link rel="stylesheet" type="text/css" href="Views/Styles/shared.css">
+        <link rel="stylesheet" type="text/css" href="Views/Styles/navbar.css">
+        <link rel="stylesheet" type="text/css" href="Views/Styles/login.css">
+    </head>
+    <body>
+    <?php
+        include_once 'Views/Shared/navbar.php';
+    ?>
+
+    <section class="loginWrapper marginAuto">
+        <div class="loginRegisterHeader textAlignCenter">
+            <header>
+                <label>Login</label>
+            </header>
+
+            <form action="/?controller=login&action=validation" method="POST">
+                <section>
+                    <div>
+                        <div class="loginRegisterInputLabelText textAlignStart widthMinContent marginAuto">
+                            <div>
+                                <label for="email">Email</label> <br/>
+                            </div>
+
+                            <input id="email" type="text" name="email" class="width100Percent" required><br>
+                        </div>
+                        <div class="loginRegisterInputLabelText textAlignStart widthMinContent marginAuto">
+                            <div>
+                                <label for="password">Password</label> <br/>
+                            </div>
+                            <input id="password" type="password" name="password" class="width100Percent" required> <br/>
+                        </div>
+
+                        <footer>
+                            <div class="forgotPassword">
+                                <a href="" >Forgot Password?</a>
+                            </div>
+                            <div class="signButtons widthMinContent marginAuto">
+                                <div class="loginSignUpButton">
+                                    <input type="submit" name="submit" class="cursorPointer width100Percent" value="Log in">
+                                </div>
+
+                                <div class="loginSignUpButton">
+                                    <a href="">
+                                        <input type="button" class="cursorPointer width100Percent" name="submit" value="Sign up">
+                                    </a>
+                                </div>
+                            </div>
+                        </footer>
+                    </div>
+                </section>
+            </form>
+        </div>
+    </section>
+    </body>
 </html>
