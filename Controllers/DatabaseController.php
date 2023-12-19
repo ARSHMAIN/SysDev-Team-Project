b3becd94bd1181bb37c12c7d75ed969d82ee78f1
ARSH5588 <arsh013@hotmail.com>
2023-12-19 12:21:44 -0500

    showing database tables done

--- Controllers/DatabaseController.php
+++ Controllers/DatabaseController.php
@@ -1,17 +1,46 @@
 <?php
 include_once 'Views/Shared/session.php';
+include_once 'database.php';
 class DatabaseController
 {
     function route(): void
     {
         global $action;
         if ($action == "index"){
-            $this->render($action);
+            $dBConnection = openDatabaseConnection();
+            $sql = "SHOW TABLES FROM snake";
+            $stmt = $dBConnection->query($sql);
+            $tableNames = $stmt->fetchAll(PDO::FETCH_ASSOC);
+            if (isset($_POST['database'])) {
+                if ($_POST['database'] === 'order') {
+                    $_POST['database'] = '`order`';
+                }
+                $table = $this->table($_POST['database']);
+                $this->render($action, ['tableNames' => $tableNames, 'table' => $table]);
+            }
+            $this->render($action, ['tableNames' => $tableNames]);
         }
     }
     function render($action, $data = []): void
     {
         extract($data);
         include_once "Views/Database/$action.php";
+    }
+
+    function table($pTableName): array
+    {
+        $dBConnection = openDatabaseConnection();
+        $sql = "DESCRIBE $pTableName";
+        $stmt = $dBConnection->query($sql);
+        $columns = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+        $sql = "SELECT * FROM $pTableName";
+        $stmt = $dBConnection->query($sql);
+        $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+        return [
+            'columns' => $columns,
+            'rows' => $rows,
+        ];
     }
 }
--- Views/Database/index.php
+++ Views/Database/index.php
@@ -1,1 +1,58 @@
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Database</title>
+</head>
+<body>
+<form id="form" action="?controller=database&action=index" method="post">
+    <label for="database"></label>
+    <select id="database" name="database">
+        <?php
+        foreach ($data['tableNames'] as $tableName) {
+            echo "<option value='" . $tableName['Tables_in_snake'] . "'>" . $tableName['Tables_in_snake'] . "</option>";
+        }
+        ?>
+    </select>
+</form>
 <?php
+if (isset($data['table'])) {
+?>
+<table>
+    <thead>
+    <tr>
+<?php
+foreach ($data['table']['columns'] as $column) {
+    echo "<th>" . $column['Field'] . "</th>";
+}
+?>
+    </tr>
+    </thead>
+    <tbody>
+    <tr>
+        <?php
+        foreach ($data['table']['rows'] as $row) {
+            echo "<tr>";
+            foreach ($row as $data) {
+                echo "<td>" . $data . "</td>";
+            }
+            echo "</tr>";
+        }
+        ?>
+    </tr>
+    </tbody>
+</table>
+<?php
+}
+?>
+</body>
+<script>
+    document.addEventListener('DOMContentLoaded', () => {
+        document.querySelector('select[name="database"]').addEventListener('change', () => {
+            document.getElementById('form').submit();
+        });
+    });
+</script>
+</html>
